493edf13b42392f0f2fd7c90d0cee238be6b9e0c
R9Cr7 <m.s.shafi2001@gmail.com>
2026-05-25 09:02:46 +0600

    remove venv and pycache from tracking

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:Jishan3169@localhost:5432/nomaddb"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
